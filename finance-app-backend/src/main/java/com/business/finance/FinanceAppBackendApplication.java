6b69f42d9fb008ebe1d2be92fb69174061b55850
multi-business-service <srinivasan.mohan@ingka.com>
2022-09-09 17:42:40 +0200

    initial changes

--- finance-app-backend/src/main/java/com/business/finance/FinanceAppBackendApplication.java
+++ finance-app-backend/src/main/java/com/business/finance/FinanceAppBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringFinanceApplication {
+public class FinanceAppBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFinanceApplication.class, args);
+		SpringApplication.run(FinanceAppBackendApplication.class, args);
 	}
 
 }
